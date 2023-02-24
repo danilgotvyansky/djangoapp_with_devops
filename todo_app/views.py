7f55aa67263c8ef5526baf2d5eefd363b5cdfa6f
danilgotvyansky <danilgotvyansky@gmail.com>
2023-02-24 14:10:56 +0100

    Added the view part; implemented Read of CRUD

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoList, ToDoItem
 
+class ListListView(ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "todo_app/todo_list.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
 # Create your views here.
