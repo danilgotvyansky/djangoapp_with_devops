f326004e2d04921841315a586845e43231a5ae15
danilgotvyansky <danilgotvyansky@gmail.com>
2023-02-24 15:48:54 +0100

    Added D to CRUD. First app version

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
     # CRUD patterns for ToDoLists
     path("list/add/", views.ListCreate.as_view(), name="list-add"),
+    path("list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"),
     # CRUD patterns for ToDoItems
     path(
         "list/<int:list_id>/item/add/",
@@ -16,5 +17,10 @@ urlpatterns = [
         "list/<int:list_id>/item/<int:pk>/",
         views.ItemUpdate.as_view(),
         name="item-update",
+    ),
+    path(
+        "list/<int:list_id>/item/<int:pk>/delete/",
+        views.ItemDelete.as_view(),
+        name="item-delete",
     ),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 from .models import ToDoList, ToDoItem
 
@@ -80,4 +81,21 @@ class ItemUpdate(UpdateView):
 
     def get_success_url(self):
         return reverse("list", args=[self.object.todo_list_id])
+
+class ListDelete(DeleteView):
+    model = ToDoList
+    # You have to use reverse_lazy() instead of reverse(),
+    # as the urls are not loaded when the file is imported.
+    success_url = reverse_lazy("index")
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
 # Create your views here.
