b49f463190f36f530fa42e9eaaf6eb8aa6f812cf
Danylo Hotvianskyi <122215118+danilgotvyansky@users.noreply.github.com>
2023-09-17 12:25:26 +0200

    Fixes, added favicon

--- vagrant/djangoapp/todo_project/settings.py
+++ vagrant/djangoapp/todo_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Initialise environment variables
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'public/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todo_app', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'public', 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
